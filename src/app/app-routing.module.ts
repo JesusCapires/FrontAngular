edb4b02c8c65d184ca573b645f99f8984df4aadb
JesusCapires <capa.2901@gmail.com>
2024-07-15 18:00:22 -0600

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
